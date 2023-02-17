6ed36b20148cb12d4422fe8725ee704cf6f83048
Collapsare <alexandr0899@mail.ru>
2023-02-18 01:31:44 +0300

    added an opportunity for resetting the game

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,84 @@
 let time = 40
 let isTimerSet = false
+let intervalID;
+let rightNumber = 1
+
 const numsList = document.querySelectorAll(".num")
 const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 const audio = document.getElementById('audio')
 const timer = document.querySelector('.timer')
+const btnReset = document.getElementById('btn-reset')
+
 shuffleArray(numbersArray)
-let rightNumber = 1
-
-
-
-
+btnReset.addEventListener('click', resetAll)
 
 numsList.forEach((item, index) => {
     item.style.color = getRandomColor()
     item.style.fontSize = getRandomFontSize(30, 91)
     item.textContent = numbersArray[index]
+
     item.addEventListener('click', rightAnswer)
     item.addEventListener('click', timeCounting)
+    item.addEventListener('click', clearListeners)
 })
 
 
+
+
+
+
+function clearListeners() {
+    if (time <= 0) {
+        numsList.forEach((item) => {
+            item.removeEventListener('click', rightAnswer)
+            item.removeEventListener('click', timeCounting)
+        })
+
+    }
+}
+
+
 function resetAll() {
+    shuffleArray(numbersArray)
+    timer.textContent = 40
+    time = 40
+    rightNumber = 1
     numsList.forEach((item, index) => {
+        item.style.backgroundColor = null
+        item.textContent = null
         item.style.color = getRandomColor()
         item.style.fontSize = getRandomFontSize(30, 91)
-        shuffleArray(numbersArray)
+
+        item.addEventListener('click', rightAnswer)
+        item.addEventListener('click', timeCounting)
+
+
         item.textContent = numbersArray[index]
     })
+    isTimerSet = false;
+    clearInterval(intervalID)
 }
 
 function timeCounting() {
     if (!isTimerSet) {
-        let intervalID = setInterval(function go() {
-        if (time <= 0) {
-            clearInterval(intervalID);
-            alert("Время закончилось");
-        }
-        
-        timer.textContent = time
-        --time
-    }, 1000)
+        intervalID = setInterval(function go() {
+            if (time <= 0) {
+                clearInterval(intervalID)
+                alert("Время закончилось")
+
+            }
+
+            timer.textContent = time
+            --time
+        }, 1000)
         isTimerSet = true
     }
-    
+
 
 }
 
 function rightAnswer(event) {
-    if (+event.target.textContent === rightNumber) {
+    if (+event.target.textContent === rightNumber && time > 0) {
 
         event.target.style.backgroundColor = 'rgb(169, 43, 43)'
         rightNumber++
