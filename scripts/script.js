b213ab8e3c0320b32b374f024a7c2b0af32b6ea2
Collapsare <alexandr0899@mail.ru>
2023-02-17 19:44:40 +0300

    added random numbers setter

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,36 @@
 const numsList = document.querySelectorAll(".num")
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+shuffleArray(numbersArray)
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+
+
+
+numsList.forEach((item, index) => {
+    item.style.color = getRandomColor()
+    item.style.fontSize = getRandomFontSize(30, 101)
+    item.textContent = numbersArray[index]
+    
+})
 
 
 function getRandomFontSize(min, max) {
     return Math.floor(Math.random() * (max - min) + min) + 'px';
 }
-numsList.forEach((item) => {
-    item.style.color = getRandomColor()
-    item.style.fontSize = getRandomFontSize(20, 101)
-})
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]
+    }
+}
+
